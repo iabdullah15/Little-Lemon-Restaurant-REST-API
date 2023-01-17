00da8238a58fe0ae3ba802bfb26f0cbd771fdfc7
Abdullah <abdullahzubair2001@gmail.com>
2023-01-18 00:02:01 +0500

    Deleted all the commented out code in the views.py file.

--- Restaurant/settings.py
+++ Restaurant/settings.py
@@ -131,6 +131,14 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+
+    'DEFAULT_FILTER_BACKENDS': [
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
     
 }
